51987434895dd5c1b2100d296430adcfc22188b3
Mike Audi <mike@mytiki.com>
2022-05-16 21:48:30 -0500

    use a public block model w/ sync & enc.
    share back sync date for a list of blocks. fixes #29

---
+++ lib/src/tiki/chain/tiki_chain_block.dart
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import 'dart:typed_data';
+
+import 'package:tiki_localchain/tiki_localchain.dart';
+import 'package:tiki_syncchain/tiki_syncchain.dart';
+
+import '../../../tiki_wallet.dart';
+
+class TikiChainBlock {
+  Uint8List? hash;
+  Uint8List? previousHash;
+  BlockContentsSchema? schema;
+  Uint8List? plaintext;
+  Uint8List? ciphertext;
+  DateTime? created;
+  DateTime? synced;
+
+  TikiChainBlock(
+      {this.hash,
+      this.previousHash,
+      this.schema,
+      this.plaintext,
+      this.ciphertext,
+      this.created,
+      this.synced});
+
+  TikiChainBlock.join(
+      {Block? block, TikiChainCacheModel? cache, DBModel? sync}) {
+    hash = cache?.hash ?? sync?.hash;
+    previousHash = block?.previousHash ?? cache?.previousHash;
+    schema = cache?.schema;
+    plaintext = cache?.contents;
+    created = block?.created ?? cache?.created;
+    ciphertext = block?.contents;
+    synced = sync?.synced;
+  }
+}
